163d46def3958b495a4464a2143fa9db98412f97
eluiui <jesusmiguez@gmail.com>
2024-09-26 14:43:45 +0200

    completo

--- src/main/java/com/campusdual/classroom/Exercise25.java
+++ src/main/java/com/campusdual/classroom/Exercise25.java
@@ -4,17 +4,24 @@ import java.util.Stack;
 
 public class Exercise25 {
     public static Stack<String> createStack() {
-
+        Stack<String> stack = new Stack<>();
+        stack.push("Smith");
+        stack.push("Montessori");
+        stack.push("Peralta");
+        stack.push("House");
+        return stack;
     }
 
     public static void printAndEmptyStack(Stack<String> stack) {
-
+        while (!stack.isEmpty()) {
+            String element = stack.pop();
+            System.out.println(element);
+        }
     }
 
     public static void main(String[] args) {
-
+        Stack<String> stack = createStack();
+        System.out.println("Primero: " + stack.peek()+"\n");
+        printAndEmptyStack(stack);
     }
-
-
-
 }
